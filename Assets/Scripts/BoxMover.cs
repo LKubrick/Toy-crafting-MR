5a9f1e64c01acd2c108b67264580e564ca4070b5
xMirnax <mirna.tominac@gmail.com>
2024-05-29 20:04:15 +0200

    New robot snaping and controlling

--- Assets/Scripts/BoxMover.cs
+++ Assets/Scripts/BoxMover.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BoxMover : MonoBehaviour
 {
-    public GameObject toyAirplaneInBox; // Reference to the toy airplane game object inside the box
-    public GameObject toyAirplaneOnTable; // Reference to the toy airplane game object on the table
-    public float riseSpeed = 1.0f; // Speed of the box rising
-    public float delayBeforeMoving = 5.0f; // Delay before the box starts moving
-    public float flipSpeed = 0.5f; // Speed of the flip animation
+    public GameObject toyInBox; 
+    public GameObject toyOnTable; 
+    public float riseSpeed = 1.0f; 
+    public float delayBeforeMoving = 5.0f; 
+    public float flipSpeed = 0.5f; 
 
     private Vector3 initialPosition;
     private Quaternion initialRotation;
@@ -21,10 +22,10 @@ public class BoxMover : MonoBehaviour
         endRotation = Quaternion.Euler(-180, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z); // Adjust for front flip
 
         // Calculate the air position based on the toy airplane's start position + 0.5 meters altitude
-        airPosition = toyAirplaneOnTable.transform.position + Vector3.up * 0.5f;
+        airPosition = toyOnTable.transform.position + Vector3.up * 0.5f;
 
         // Set the toy airplane on the table to inactive at the start
-        toyAirplaneOnTable.SetActive(false);
+        toyOnTable.SetActive(false);
 
         StartCoroutine(StartMovingAfterDelay());
     }
@@ -52,9 +53,9 @@ public class BoxMover : MonoBehaviour
         transform.rotation = endRotation;
 
         // Make the toy airplane on the table visible after the flip
-        toyAirplaneOnTable.SetActive(true);
+        toyOnTable.SetActive(true);
         // Make the toy airplane in the box inactive
-        toyAirplaneInBox.SetActive(false);
+        toyInBox.SetActive(false);
 
         StartCoroutine(ReturnToInitialPosition());
     }
